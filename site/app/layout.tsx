765841ef8c22c7f57a8fb764085dee7c9e006f03
Alex Hunt <hello@alexhunt.io>
2024-05-28 21:57:30 +0200

    Add @vercel/analytics

--- site/app/layout.tsx
+++ site/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Hero from '../components/Hero.tsx';
@@ -37,6 +38,7 @@ export default function RootLayout({
             <div className="md:max-w-560">{children}</div>
           </main>
         </div>
+        <Analytics />
       </body>
     </html>
   );
