8d22dd6815dfa4372e72ca5ed44d54b5825720d7
Alex Hunt <hello@alexhunt.dev>
2025-07-04 20:49:12 +0100

    Fix content caching across requests (#2)

--- site/content/cacheFeedImage.tsx
+++ site/content/cacheFeedImage.tsx
@@ -2,6 +2,7 @@ import type { PageObjectResponse } from '@notionhq/client/build/src/api-endpoint
 
 import { list, put } from '@vercel/blob';
 import invariant from 'invariant';
+import { unstable_cache } from 'next/cache';
 import { createHash } from 'node:crypto';
 import path from 'node:path';
 import openGraph from 'open-graph-scraper';
@@ -11,58 +12,60 @@ const EMPTY_CONTENT_MARKER = 'no-content.json';
 /**
  * Cache the preview image for the given feed item on the server/CDN.
  */
-export default async function cacheFeedImage(
-  page: PageObjectResponse,
-): Promise<string | null> {
-  const imageKey = createHash('sha256')
-    .update(page.id + '-' + page.last_edited_time)
-    .digest('hex')
-    .slice(0, 16);
-  const cachePrefix = 'feed_v2/' + imageKey;
+const cacheFeedImage = unstable_cache(
+  async (page: PageObjectResponse): Promise<string | null> => {
+    const imageKey = createHash('sha256')
+      .update(page.id + '-' + page.last_edited_time)
+      .digest('hex')
+      .slice(0, 16);
+    const cachePrefix = 'feed_v2/' + imageKey;
 
-  // Check for existing blob
-  const {
-    blobs: [cachedImage],
-  } = await list({ prefix: cachePrefix });
+    // Check for existing blob
+    const {
+      blobs: [cachedImage],
+    } = await list({ prefix: cachePrefix });
 
-  if (cachedImage != null) {
-    return path.basename(cachedImage.pathname) !== EMPTY_CONTENT_MARKER
-      ? cachedImage.url
-      : null;
-  }
+    if (cachedImage != null) {
+      return path.basename(cachedImage.pathname) !== EMPTY_CONTENT_MARKER
+        ? cachedImage.url
+        : null;
+    }
 
-  let sourceImageUrl = await resolveFeedItemPreview(page);
-  if (sourceImageUrl == null) {
-    await put(cachePrefix + '/' + EMPTY_CONTENT_MARKER, '{}', {
-      access: 'public',
-      addRandomSuffix: false,
-    });
-    return null;
-  }
+    let sourceImageUrl = await resolveFeedItemPreview(page);
+    if (sourceImageUrl == null) {
+      await put(cachePrefix + '/' + EMPTY_CONTENT_MARKER, '{}', {
+        access: 'public',
+        addRandomSuffix: false,
+      });
+      return null;
+    }
 
-  // Fetch remote image
-  const response = await fetch(sourceImageUrl, { cache: 'no-store' });
-  if (!response.ok) {
-    console.warn('Failed to fetch image:', sourceImageUrl);
-    return null;
-  }
+    // Fetch remote image
+    const response = await fetch(sourceImageUrl, { cache: 'no-store' });
+    if (!response.ok) {
+      console.warn('Failed to fetch image:', sourceImageUrl);
+      return null;
+    }
 
-  const cachePath =
-    cachePrefix + '/preview' + path.extname(new URL(sourceImageUrl).pathname);
+    const cachePath =
+      cachePrefix + '/preview' + path.extname(new URL(sourceImageUrl).pathname);
 
-  try {
-    // Upload to Vercel Blob
-    const result = await put(cachePath, response.body!, {
-      access: 'public',
-      addRandomSuffix: false,
-    });
+    try {
+      // Upload to Vercel Blob
+      const result = await put(cachePath, response.body!, {
+        access: 'public',
+        addRandomSuffix: false,
+      });
 
-    return result.url;
-  } catch (e) {
-    console.error('Failed to process image:', e);
-    return null;
-  }
-}
+      return result.url;
+    } catch (e) {
+      console.error('Failed to process image:', e);
+      return null;
+    }
+  },
+);
+
+export default cacheFeedImage;
 
 async function resolveFeedItemPreview(
   page: PageObjectResponse,
--- site/content/getProperties.tsx
+++ site/content/getProperties.tsx
@@ -2,39 +2,46 @@
 
 import { isFullPage } from '@notionhq/client';
 import invariant from 'invariant';
-import { cache } from 'react';
+import { unstable_cache } from 'next/cache';
 import { notion } from './notion';
 
 const PROPERTIES_DATABASE_ID = '57bb578c629a411695fb9df41600b0af';
+const CACHE_DURATION = 60 * 5; // 5 minutes
 
-const getProperties = cache(async (): Promise<Record<string, string>> => {
-  const response = await notion.databases.query({
-    database_id: PROPERTIES_DATABASE_ID,
-  });
-  const properties: Record<string, string> = {};
+const getProperties = unstable_cache(
+  async (): Promise<Record<string, string>> => {
+    const response = await notion.databases.query({
+      database_id: PROPERTIES_DATABASE_ID,
+    });
+    const properties: Record<string, string> = {};
 
-  for (const page of response.results) {
-    if (!isFullPage(page)) {
-      continue;
+    for (const page of response.results) {
+      if (!isFullPage(page)) {
+        continue;
+      }
+
+      invariant(
+        page.properties['Key']?.type === 'title',
+        'Expected property "Key" to be of type "title"',
+      );
+      invariant(
+        page.properties['Value']?.type === 'rich_text',
+        'Expected property "Value" to be of type "rich_text"',
+      );
+
+      const key = page.properties['Key'].title[0].plain_text;
+
+      if (key != null) {
+        properties[key] = page.properties['Value'].rich_text[0].plain_text;
+      }
     }
 
-    invariant(
-      page.properties['Key']?.type === 'title',
-      'Expected property "Key" to be of type "title"',
-    );
-    invariant(
-      page.properties['Value']?.type === 'rich_text',
-      'Expected property "Value" to be of type "rich_text"',
-    );
-
-    const key = page.properties['Key'].title[0].plain_text;
-
-    if (key != null) {
-      properties[key] = page.properties['Value'].rich_text[0].plain_text;
-    }
-  }
-
-  return properties;
-});
+    return properties;
+  },
+  [],
+  {
+    revalidate: CACHE_DURATION,
+  },
+);
 
 export default getProperties;
